56f4ed667b43f715cc600129df5831f71a23e88b
Swetha Devaraj Urs <32193744+swethiurs@users.noreply.github.com>
2017-10-26 23:06:26 -0700

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+package sort;
+
+public class BubbleSort {
+	
+	public static void main(String args[]) {
+		int[] arr = new int[] {5,9,8,1,10,30,24};
+		int n = arr.length;
+		for(int i=0;i<n-1;i++) {
+			for(int j=0;j<n-i-1;j++) {
+				if(arr[j]>arr[j+1])
+				{
+				    int temp = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		
+	    for(int i=0;i<n-1;i++) {
+	    	System.out.println(arr[i] + " ");
+	    }
+	}
+
+}
